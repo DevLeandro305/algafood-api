f2adeedaf67c675ff9ccee772d7b682079933548
Leandro Soares <lfsoares.dev@gmail.com>
2024-11-29 10:35:34 -0300

    Desambiguação com múltiplos beans

--- algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/di/notificacao/NotificadorSMS.java
+++ algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/di/notificacao/NotificadorSMS.java
@@ -4,11 +4,12 @@ import com.lfsoares.algafood.di.modelo.Cliente;
 import org.springframework.stereotype.Component;
 
 @Component
-public class NotificadorEmail implements Notificador{
+public class NotificadorSMS implements Notificador{
 
     @Override
     public void notificar(Cliente cliente, String mensagem) {
-        System.out.printf("Notificando %s através do e-mail %s: %s\n",
-                cliente.getNome(), cliente.getEmail(), mensagem);
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
     }
+
 }
--- algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/di/service/AtivacaoClienteService.java
+++ algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/di/service/AtivacaoClienteService.java
@@ -2,20 +2,23 @@ package com.lfsoares.algafood.di.service;
 
 import com.lfsoares.algafood.di.modelo.Cliente;
 import com.lfsoares.algafood.di.notificacao.Notificador;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public class AtivacaoClienteService {
 
-    private Notificador notificador;
-
-    public AtivacaoClienteService(Notificador notificador) {
-        this.notificador = notificador;
-
-        System.out.println("AtivacaoClienteService: " + notificador);
-    }
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        for (Notificador notificador : notificadores) {
+            notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+        }
+
     }
 }
