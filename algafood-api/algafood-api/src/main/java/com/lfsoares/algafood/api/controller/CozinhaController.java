6e6961236e92fa6ced98452d0be9b02f7485ffe8
Leandro Soares <lfsoares.dev@gmail.com>
2024-12-03 11:46:58 -0300

    Modelando as primeiras Collection Resources com REST

---
+++ algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,24 @@
+package com.lfsoares.algafood.api.controller;
+
+import com.lfsoares.algafood.domain.model.Cozinha;
+import com.lfsoares.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+    @GetMapping
+    public List<Cozinha> listar() {
+        return cozinhaRepository.listar();
+    }
+}
