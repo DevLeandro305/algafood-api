8847f71030d993c677804ea3a8abcf979a3d63d1
Leandro Soares <lfsoares.dev@gmail.com>
2024-12-04 12:09:53 -0300

    Manipulando a resposta HTTP com ResponseEntity e a anotação @ResponseStatus

--- algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/api/controller/CozinhaController.java
+++ algafood-api/algafood-api/src/main/java/com/lfsoares/algafood/api/controller/CozinhaController.java
@@ -4,11 +4,11 @@ import com.lfsoares.algafood.api.model.CozinhasXmlWrapper;
 import com.lfsoares.algafood.domain.model.Cozinha;
 import com.lfsoares.algafood.domain.repository.CozinhaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,6 +19,7 @@ public class CozinhaController {
     @Autowired
     private CozinhaRepository cozinhaRepository;
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Cozinha> listar() {
         return cozinhaRepository.listar();
@@ -30,7 +31,18 @@ public class CozinhaController {
     }
 
     @GetMapping("/{id}")
-    public Cozinha buscar(@PathVariable Long id) {
-        return cozinhaRepository.buscar(id);
+    public ResponseEntity<Cozinha> buscar(@PathVariable Long id) {
+        Cozinha cozinha =  cozinhaRepository.buscar(id);
+
+      //return ResponseEntity.status(HttpStatus.OK).body(cozinha);
+      //return ResponseEntity.ok(cozinha);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.LOCATION, "http://localhost:8080/cozinhas");
+
+        return ResponseEntity
+                .status(HttpStatus.FOUND)
+                .headers(headers)
+                .build();
     }
 }
